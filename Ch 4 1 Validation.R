f6f2ce0936dbd088841595ccbd7818d5c7b24c15
Ferenc Szalma <szalmaf@users.noreply.github.com>
2016-04-23 16:04:11 -0400

    Add loocv slow and fast calculation

--- Ch 4 1 Validation.R
+++ Ch 4 1 Validation.R
@@ -3,3 +3,23 @@ require(boot) # Bootstrap resampling package
 ?boot
 ?cv.glm # Get estimated k-fold cross-validation prediction error for genaralized linear models
 plot(mpg~horsepower, data= Auto)
+
+View(Auto)
+
+## LOOVC - Leave one out cross validation
+glm.fit = glm(mpg~horsepower, data=Auto) # If family is specified, fits a linear model
+cv.glm(Auto, glm.fit)$delta # Brute force fitting; Slow!!!
+
+## LOOCV - fast algorithm hand coded
+fit = glm.fit
+loocv = function(fit){
+  h = lm.influence(fit)$h
+  mean((residuals(fit)/(1-h))^2)  
+}
+
+loocv(fit)
+
+
+
+
+
