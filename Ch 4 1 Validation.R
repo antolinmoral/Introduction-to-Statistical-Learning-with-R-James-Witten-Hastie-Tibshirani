3a431d999afd0fd1b60fb2a1e17d7ee3f672bd88
Ferenc Szalma <szalmaf@users.noreply.github.com>
2016-04-23 17:38:23 -0400

    Add bootstrap calculation

--- Ch 4 1 Validation.R
+++ Ch 4 1 Validation.R
@@ -36,3 +36,28 @@ for(d in degree){
 lines(degree, cv.error10, type="b", col="red")
 
 
+## Bootstrap
+## Minimum risk investment - Section 5.2
+
+alpha = function(x,y){
+  vx = var(x)
+  vy = var(y)
+  cxy = cov(x,y)
+  (vy - cxy)/(vx+vy-2*cxy)
+}
+alpha(Portfolio$X, Portfolio$Y)
+
+## What is the standard error on alpha?
+alpha.fn = function(data, index){
+  with(data[index,], alpha(X, Y)) # Take dataframe, here data[index,], and use its columns in functions, here alpha.
+}
+
+alpha.fn(Portfolio, 1:100)
+
+set.seed(1)
+alpha.fn(Portfolio, sample(1:100, 100, replace=TRUE))
+
+boot.out = boot(Portfolio, alpha.fn, R=1000)
+boot.out
+plot(boot.out)
+
